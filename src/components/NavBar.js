0b94c7c585bb829e42fb08b21c6245a6555630ca
Eric Maranga <ekirira22@gmail.com>
2024-04-17 18:36:38 +0300

    worked on navbar, added responsiveness

---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../DigressEvents.png";
+import { Container, Nav, Navbar, Button, Offcanvas } from "react-bootstrap";
+
+export default function NavBar(){
+    /* Offcanvas Setters*/
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Navbar className="p-4 opacity-75 fs-6 text-light" bg="primary" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={logo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        Digress Appointments
+                    </Navbar.Brand>
+                    
+                    <Button className="shadow-none d-lg-none border-0 fs-4" onClick={handleShow}>
+                        <span className="navbar-toggler-icon"></span>
+                    </Button>
+
+                    <Offcanvas show={show} onHide={handleClose} responsive="lg">
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title>Digress Appointments</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav>
+                                <Nav.Link className="fw-bold" href="#home">HOME</Nav.Link>
+                                <Nav.Link className="fw-bold" href="#about">ABOUT</Nav.Link>
+                                <Nav.Link className="fw-bold" href="#services">SERVICES</Nav.Link>
+                                <Nav.Link className="fw-bold" href="#contact">CONTACT</Nav.Link>
+
+                            </Nav>
+                            <Nav className="ms-4">
+                                <Button variant="success">SIGN UP</Button>
+                                <Button className="ms-2" variant="danger">LOG IN</Button>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Offcanvas>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
